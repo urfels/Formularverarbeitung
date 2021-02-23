c14f3b32bd84155a2fb541b4d833e0c1756616f3
urfels <jannes.urfels@gmail.com>
2021-02-23 13:13:34 +0100

    finale Version

--- js/services/employeeService.js
+++ js/services/employeeService.js
@@ -11,31 +11,61 @@ class EmployeeService {
     // parse json string to class Emplyee
     try {
       var empl = JSON.parse(employee);
-      var newEmployee = new Employee(empl.id, empl.name, empl.email, empl.role);
+      var newEmployee = new Employee(empl.id, empl.name, empl.email, empl.role, empl.lastModified);
       if(newEmployee instanceof Employee){
         console.log("JO Mitarbeiter wiederhergestellt");
-        var alle_zeilen = document.getElementById("userContent").innerHTML;
-        var row = "<div id="+ newEmployee.id +" class='row'>"+
-           "<div class='col'>" + newEmployee.id + "</div>"+
-           "<div class='col'>" + newEmployee.name + "</div>"+
-           "<div class='col'>" + newEmployee.email + "</div>"+
-           "<div class='col'>" + newEmployee.role + "</div>"+
-           "<div class='col'><button class='button' onClick='EmployeeService.deleteEmployee(event)'><i id="+newEmployee.id+" class='fa fa-trash'></i></button>   </div>"+
-           "</div>"
-
-           console.log(document);
-        alle_zeilen = alle_zeilen + row;
-        document.getElementById("userContent").innerHTML = alle_zeilen;
+         EmployeeService.createEmployeeLine(newEmployee);
       }
     } catch (e) {
       console.log(e);
     }
   }
+}
+
+static changeEmployee(){
+  var id = document.getElementById("id_dialog_id").value;
+  var name = document.getElementById("id_dialog_name").value;
+  var email = document.getElementById("id_dialog_e-mail").value;
+  var role = document.getElementById("id_dialog_role").value;
+  var lastModified = new Date();
+  console.log("change: "+lastModified);
+
+  var employee = new Employee(id, name, email, role, lastModified);
+  window.localStorage.setItem(id, JSON.stringify(employee));
+
+  var div = document.getElementById(id);
+  console.log(div);
+  div.remove();
+
+  EmployeeService.createEmployeeLine(employee);
+}
+
+  static processForm(event){
+    var empl = JSON.parse(window.localStorage.getItem(event.target.id));
+    document.getElementById("id_dialog_name").value = empl.name;
+    document.getElementById("id_dialog_e-mail").value = empl.email;
+    document.getElementById("id_dialog_role").value = empl.role;
+    document.getElementById("id_dialog_id").value = empl.id;
   }
 
+static createEmployeeLine(employee){
+  var alle_zeilen = document.getElementById("userContent").innerHTML;
+  var row = "<div id="+ employee.id +" class='row'>"+
+     "<div class='col'>" + employee.id + "</div>"+
+     "<div class='col'>" + employee.name + "</div>"+
+     "<div class='col'>" + employee.email + "</div>"+
+     "<div class='col'>" + employee.role + "</div>"+
+     "<div class='col'>" + moment(employee.lastModified).format('DD.MM.YYYY HH:mm:ss') + "</div>"+
+     "<div class='col'>"+
+        "<button class='button' onClick='EmployeeService.deleteEmployee(event)'><i id="+employee.id+" class='fa fa-trash'></i></button>"+
+        "<button class='button' data-bs-toggle='modal' data-bs-target='#exampleModal' onClick='EmployeeService.processForm(event)'><i id="+employee.id+" class='fa fa-wrench'></i></button>"+
+        "</div>"+
+     "</div>"
 
-
-
+     console.log(document);
+  alle_zeilen = alle_zeilen + row;
+  document.getElementById("userContent").innerHTML = alle_zeilen;
+}
 
   static deleteEmployee(event){
     console.log("Event:"+event.target.id);
@@ -43,10 +73,8 @@ class EmployeeService {
     var myLocalStorage = window.localStorage;
     myLocalStorage.removeItem(event.target.id);
 
-
     var idToDelete = event.target.id;
     var myobj = document.getElementById(idToDelete);
-
     myobj.remove();
   }
 
@@ -56,54 +84,17 @@ class EmployeeService {
     var email = document.getElementById("id_e-mail").value;
     var role = document.getElementById("id_role").value;
     var id = new Date().getTime();
-
-    var employee = new Employee(id, name, email, role);
+    var lastModified = new Date();
+  console.log("add: "+lastModified);
+    var employee = new Employee(id, name, email, role, lastModified);
     console.log(employee);
 
     var myLocalStorage = window.localStorage;
     myLocalStorage.setItem(employee.id, JSON.stringify(employee) );
 
-   var alle_zeilen = document.getElementById("userContent").innerHTML;
-   var row = "<div id="+ employee.id +" class='row'>"+
-      "<div class='col'>" + employee.id + "</div>"+
-      "<div class='col'>" + employee.name + "</div>"+
-      "<div class='col'>" + employee.email + "</div>"+
-      "<div class='col'>" + employee.role + "</div>"+
-      "<div class='col'><button class='button' onClick='EmployeeService.deleteEmployee(event)'><i id="+employee.id+" class='fa fa-trash'></i></button>   </div>"+
-      "</div>"
+   EmployeeService.createEmployeeLine(employee);
 
-      console.log(document);
-   alle_zeilen = alle_zeilen + row;
-   document.getElementById("userContent").innerHTML = alle_zeilen;
 
-/**
-    var div = document.createElement("div");
-    div.setAttribute("class", "row");
-    var col = document.createElement("div");
-    col.setAttribute("class", "col");
-    col.innerHTML = employee.id;
-      div.appendChild(col);
-    var col2 = document.createElement("div");
-    col2.setAttribute("class", "col");
-    col2.innerHTML = employee.name;
-    div.appendChild(col2);
-    var col3 = document.createElement("div");
-    col3.setAttribute("class", "col");
-    col3.innerHTML = employee.email;
-    div.appendChild(col3);
-    var col4 = document.createElement("div");
-    col4.setAttribute("class", "col");
-    col4.innerHTML = employee.role;
-    div.appendChild(col4);
-    var col5 = document.createElement("div");
-    col5.setAttribute("class", "col");
-    col5.innerHTML = "<input type='button' value='Löschen'>";
-    div.appendChild(col5);
-    var table = document.getElementById("table");
-    table.appendChild(div);
-**/
-    //document.body.insertBefore(document.getElementById("userContent"), div);
-    //document.getElementById("userContent").innerHTML = row;
   }
 
   static roleChange(event){
